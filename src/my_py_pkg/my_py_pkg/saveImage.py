ec5a4c8f78eb631680f646572fbf066283e4d774
adri-gth <aldoadrian050301@gmail.com>
2024-02-20 13:17:06 +0000

    20/02/2024

--- src/my_py_pkg/my_py_pkg/saveImage.py
+++ src/my_py_pkg/my_py_pkg/saveImage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import rclpy 
 from rclpy.node import Node 
-from sensor_msgs.msg import CompressedImage 
+from sensor_msgs.msg import Image 
 import cv_bridge 
 import cv2 
 import numpy as np
@@ -11,13 +11,16 @@ class ImageToVideoConverter(Node):
     def __init__(self): 
         super().__init__('image_to_video_converter') 
         self.bridge = cv_bridge.CvBridge() 
-        self.subscription = self.create_subscription( CompressedImage, '/image_raw/compressed', self.image_callback, 10 ) 
+        self.subscription = self.create_subscription( Image, '/front_camera/image_raw', self.image_callback, 10 ) 
         self.video_writer = None
-
+    
     def image_callback(self, msg):
+        print("Received an image!")
         try:
-            np_arr = np.frombuffer(msg.data, np.uint8)
-            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            cv_image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+            #np_arr = np.frombuffer(msg.data, np.uint8)
+            #cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
             if self.video_writer is None:
                 self.init_video_writer(cv_image)
             self.video_writer.write(cv_image)
@@ -28,15 +31,17 @@ class ImageToVideoConverter(Node):
         try:
             height, width, _ = image.shape
             video_format = 'mp4'  # or any other video format supported by OpenCV
-            video_filename = 'output_video.' + video_format
+            video_filename = '/home/data/polytunnel_dataset/video_rgb/output_video.' + video_format
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            
             fps = 30  # Frames per second
-            self.video_writer = cv2.VideoWriter(video_filename, fourcc, fps, (width, height))
+            self.video_writer = cv2.VideoWriter(video_filename, fourcc, fps, (width, height))            
+            
         except Exception as e:
             self.get_logger().error('Error initializing video writer: %s' % str(e))
 
-
     def destroy_node(self):
+        print("Releasing VideoWrite")
         if self.video_writer is not None:
             self.video_writer.release()
         super().destroy_node()
---
+++ src/my_py_pkg/my_py_pkg/savePointCloud.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs_py.point_cloud2 as pc2
+import open3d as o3d
+import numpy as np
+
+class PointCloudToPCDConverter(Node):
+    def __init__(self):
+        super().__init__('point_cloud_to_pcd_converter')
+        self.subscription = self.create_subscription(PointCloud2, '/front_camera/points', self.points_callback, 10)
+
+    def points_callback(self, msg):
+        # Convertir PointCloud2 a un array de Numpy
+        points_list = list(pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True))
+        np_points = np.array(points_list, dtype=np.float64)
+
+        # Crear una nube de puntos con Open3D
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(np_points)
+
+        # Guardar la nube de puntos en un archivo .pcd
+        o3d.io.write_point_cloud("/home/data/polytunnel_dataset/point_clouds/output.pcd", pcd)
+        self.get_logger().info("PointCloud saved to '/home/data/polytunnel_dataset/point_clouds/output.pcd'")
+
+def main(args=None):
+    rclpy.init(args=args)
+    point_cloud_to_pcd_converter = PointCloudToPCDConverter()
+    rclpy.spin(point_cloud_to_pcd_converter)
+    point_cloud_to_pcd_converter.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
